bd2faf35739380e8d31df6b72829e002cf10a71a
Jonathan Calvert <jcalvert@taximagic.com>
2014-06-20 12:19:54 -0400

    Add lock and release routes + tests

--- app/controllers/lock_controller.rb
+++ app/controllers/lock_controller.rb
@@ -3,21 +3,49 @@ class LockController < ApplicationController
   def lock_status
     params.require(:app)
     params.require(:environment)
-    respond_to do |format| 
-      format.json do
-        @app=App.find_by_name(params[:app])
-        @env=Environment.find_by_name(params[:environment])
-        if @app.nil? or @env.nil?
-          render :json => {error: "Invalid app/env"}, :status => 500
-        else
-          lock=Lock.where(:app => @app, :environment => @env, :active => true).first
-          if lock.nil?
-            render :json => {locked: false}
-          else
-            render :json => {locked: true, at: lock.created_at, by: lock.user.name}
-          end
-        end
+    if get_app_env
+      lock=Lock.where(:app => @app, :environment => @env, :active => true).first
+      if lock.nil?
+        render :json => {locked: false}
+      else
+        render :json => {locked: true, at: lock.created_at, by: lock.user.name}
       end
+    end
+  end
+
+  def create
+    params.require(:app)
+    params.require(:environment)
+    params.require(:user)
+    if get_app_env
+      @user = User.find_or_create_by(:name => params[:user])
+      release_all_locks
+      @lock = Lock.create(:user => @user, :environment => @env, :app => @app)
+      head 200
+    end
+  end
+
+  def release
+    params.require(:app)
+    params.require(:environment)
+    if get_app_env
+      release_all_locks
+      head 200
+    end
+  end
+
+  private
+  def get_app_env
+    @app=App.find_by_name(params[:app])
+    @env=Environment.find_by_name(params[:environment])
+    not_found=(@app.nil? or @env.nil?)
+    render :json => {error: "Invalid app/env"}, :status => 500 if not_found
+    return !not_found
+  end
+
+  def release_all_locks
+    Lock.where(:app => @app, :environment => @env, :active => true).each do |lock|
+      lock.release!
     end
   end
 
--- test/controllers/lock_controller_test.rb
+++ test/controllers/lock_controller_test.rb
@@ -1,13 +1,13 @@
 require 'test_helper'
 
 class LockControllerTest < ActionController::TestCase
+  setup do
+    @env=Environment.create(name: "trunk")
+    @app=App.create(name: "foobar")
+    @user=User.create(name: "bob")
+  end
 
   context "on GET for lock_status" do
-    setup do
-      @env=Environment.create(name: "trunk")
-      @app=App.create(name: "foobar")
-      @user=User.create(name: "bob")
-    end
 
     should 'return true if there is an active lock' do
       @lock=Lock.create(environment: @env, app: @app, user: @user)
@@ -37,7 +37,7 @@ class LockControllerTest < ActionController::TestCase
       assert_raise ActionController::ParameterMissing do
         get :lock_status, format: :json, environment: "foobar"
       end
-      assert_raise ActionController::ParameterMissing do 
+      assert_raise ActionController::ParameterMissing do
         get :lock_status, format: :json, app: "foobar"
       end
     end
@@ -47,6 +47,85 @@ class LockControllerTest < ActionController::TestCase
       assert_response :error
       result={"error" => "Invalid app/env"}
       assert_equal result, JSON.parse(@response.body.to_s)
+    end
+  end
+
+  context "on POST to lock" do
+
+    should 'return an error without app, env and user' do
+      assert_raise ActionController::ParameterMissing do
+        post :create, format: :json, environment: "foobar", user: "foobar"
+      end
+      assert_raise ActionController::ParameterMissing do
+        post :create, format: :json, app: "foobar", user: "foobar"
+      end
+      assert_raise ActionController::ParameterMissing do
+        post :create, format: :json, app: "foobar", environment: "foobar"
+      end
+    end
+
+    should 'return an error if there is no such app or env' do
+      post :create, format: :json, environment: "foobusy", app: @app.name, user: "foobar"
+      assert_response :error
+      result={"error" => "Invalid app/env"}
+      assert_equal result, JSON.parse(@response.body.to_s)
+    end
+
+    should "create a user if it doesn't exist" do
+      name="foobar_#{Time.now.to_s}"
+      post :create, format: :json, environment: @env.name, app: @app.name, user: name
+      assert_response :success
+      assert_equal true, !User.where(:name => name).empty?
+    end
+
+    should "lock an app/env" do
+      post :create, format: :json, environment: @env.name, app: @app.name, user: @user.name
+      assert_response :success
+      @lock = assigns(:lock)
+      assert_not_nil @lock
+      assert_equal @env, @lock.environment
+      assert_equal @app, @lock.app
+      assert_equal @user, @lock.user
+      assert @lock.active?
+    end
+
+    should "force an override of an existing lock" do
+      @old_lock=Lock.create(environment: @env, app: @app, user: @user)
+      assert @old_lock.active?
+      post :create, format: :json, environment: @env.name, app: @app.name, user: "foobar"
+      assert_response :success
+      @lock = assigns(:lock)
+      assert_not_nil @lock
+      assert_equal @env, @lock.environment
+      assert_equal @app, @lock.app
+      assert_equal "foobar", @lock.user.name
+      assert @lock.active?
+      assert !Lock.find(@old_lock.id).active?
+    end
+  end
+
+  context "on PUT to lock" do
+    should 'return an error without both app and env' do
+      assert_raise ActionController::ParameterMissing do
+        put :release, format: :json, environment: "foobar"
+      end
+      assert_raise ActionController::ParameterMissing do
+        put :release, format: :json, app: "foobar"
+      end
+    end
+
+    should 'return an error if there is no such thing' do
+      put :release, format: :json, environment: "foobusy", app: @app.name
+      assert_response :error
+      result={"error" => "Invalid app/env"}
+      assert_equal result, JSON.parse(@response.body.to_s)
+    end
+
+    should "release a lock" do
+      @old_lock=Lock.create(environment: @env, app: @app, user: @user)
+      put :release, format: :json, environment: @env.name, app: @app.name
+      assert_response :success
+      assert !Lock.find(@old_lock.id).active?
     end
   end
 end
