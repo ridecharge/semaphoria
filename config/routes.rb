edf80721c90934d9c6c76faec1e3ab25d7234b90
Jonathan Calvert <jcalvert@taximagic.com>
2014-06-27 16:59:52 -0400

    Add delete route; we're really updating the lock object for history purposes, but it is effectively removed

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   get 'lock_status' => 'lock'
   post 'lock' => 'lock#create'
   put 'lock' => 'lock#release'
+  delete 'lock' => 'lock#release'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
