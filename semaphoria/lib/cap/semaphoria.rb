e3f64e3c468770e019030893238571d74e979321
Tim Snowhite <tsnowhite@taximagic.com>
2014-07-07 20:15:35 -0400

    [cap_recipe] (reengineering: extract class, extract recipe) fix a bunch of typos.

--- semaphoria/lib/cap/semaphoria.rb
+++ semaphoria/lib/cap/semaphoria.rb
@@ -1,21 +1,20 @@
 require 'semaphoria'
 class Semaphoria::CapCheck < Struct.new(:app_name,:env_name,:user, :logger,:endpoint)
+  def self.ask
+    response = Capistrano::CLI.ui.ask "Are you sure you wish to deploy anyway? Type Y to continue."
+    abort "exiting" unless response.chomp.upcase =="Y"
+  end
   def self.create(cap)
     @semaphore = Off.new(cap.logger)
     return self unless cap.fetch(:semaphoria_check_lock, true)
-    @semaphore=Semaphoria::CapCheck.new(cap.fetch(:app_name),cap.fetch(:env_name), Etc.getlogin, cap.logger).status
+    @semaphore=Semaphoria::CapCheck.new(cap.fetch(:app_name),cap.fetch(:env_name), Etc.getlogin, cap.logger)
     self
   end
   def self.check_lock
     @semaphore.have_lock? || @semaphore.locked?
-    self
   end
   def self.force_lock
     return if @semaphore.have_lock?
-    if @semaphore.locked?
-      response = Capistrano::CLI.ui.ask "Are you sure you wish to deploy anyway? Type Y to continue."
-      abort "exiting" unless response.chomp.upcase =="Y"
-    end
     @semaphore.lock
   end
   def self.force_unlock
@@ -23,15 +22,12 @@ class Semaphoria::CapCheck < Struct.new(:app_name,:env_name,:user, :logger,:endp
   end
   def self.lock_if_unlocked
     return if @semaphore.have_lock?
-    if @semaphore.locked?
-      response = Capistrano::CLI.ui.ask "Are you sure you wish to deploy anyway? Type Y to continue."
-      abort "exiting" unless response.chomp.upcase =="Y"
-    end
+    ask if @semaphore.locked?
     @semaphore.lock
   end
   def self.unlock_my_lock
-    return unless @semaphore.have_lock?
-    @semaphore.unlock
+    return @semaphore.unlock if @semaphore.have_lock?
+    @semaphore.locked?
   end
 
 
@@ -59,21 +55,24 @@ class Semaphoria::CapCheck < Struct.new(:app_name,:env_name,:user, :logger,:endp
     @semaphore = Semaphoria::Client.new(:host => endpoint.host, :scheme => endpoint.scheme)
   end
   def status
+    return @response if @response
     logger.info "Checking environment locks..."
-    @response ||= @semaphore.lock_status(app_name, env_name)
-    self
+    @response = @semaphore.lock_status(app_name, env_name)
   end
 
   def have_lock?
     if status.locked_by == user
-      logger.info "You (#{user}) locked this app:environment at #{status_response.locked_at} - remember to release the lock when you're done."
+      logger.info "You (#{user}) locked this app:environment at #{status.locked_at} - remember to release the lock when you're done."
     end
     status.locked_by == user
   end
   def locked?
-    return unless @response.successful? #unlocked when unreachable
+    unless status.successful? #unlocked when unreachable
+      logger.info "Failed to access semaphoria server, unknown lock state."
+      return
+    end
     if status.locked?
-    logger.info "This app:environment was locked by #{status_response.locked_by} at #{status_response.locked_at}."
+      logger.info "This app:environment was locked by #{status.locked_by} at #{status.locked_at}."
     else
       logger.info "This app:environment is currently unlocked."
     end
@@ -84,34 +83,34 @@ class Semaphoria::CapCheck < Struct.new(:app_name,:env_name,:user, :logger,:endp
     if resp.successful?
       logger.info "Locked app:environment for #{user} - remember to release the lock when you're done"
     else
-      logger.info "Failed to lock app:environment for #{user}: #{resp.code} #{resp.body.inspect}"
+      logger.info "Failed to lock app:environment for #{user}: #{resp.status} #{resp.errors.join(" ")}"
     end
     self
   end
   def unlock
     resp = @semaphore.unlock(app_name, env_name)
-    logger.info "Lock released for app:environment: #{resp.code} #{resp.body.inspect}"
+    logger.info "Lock released for app:environment. #{resp.status} #{resp.errors.join(" ")}"
     self
   end
 end
 
 
-
-set(:semaphoria_check_lock, fetch(:semaphoria_check_lock, fetch(:check_lock_status, true)))
-before "deploy", "semaphoria:lock"
-
-namespace "semaphoria" do
-  task "check_lock" do
-    Semaphoria::CapCheck.create(cap).check_lock
-  end
-  task "lock" do
-    Semaphoria::CapCheck.create(cap).lock_if_unlocked
-  end
-  task "unlock" do
-    Semaphoria::CapCheck.create(cap).unlock_my_lock
-  end
-  task "force_unlock" do
-    Semaphoria::CapCheck.create(cap).force_unlock
+Capistrano::Configuration.instance(:must_exist).load do
+  set(:semaphoria_check_lock, fetch(:semaphoria_check_lock, fetch(:check_lock_status, true)))
+  before "deploy", "semaphoria:lock"
+  
+  namespace "semaphoria" do
+    task "check_lock" do
+      Semaphoria::CapCheck.create(self).check_lock
+    end
+    task "lock" do
+      Semaphoria::CapCheck.create(self).lock_if_unlocked
+    end
+    task "unlock" do
+      Semaphoria::CapCheck.create(self).unlock_my_lock
+    end
+    task "force_unlock" do
+      Semaphoria::CapCheck.create(self).force_unlock
+    end
   end
 end
-
